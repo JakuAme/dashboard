d2e2f58e9d556441cc8f8e395521327838be2d70
Jakuame <135069404+JakuAme@users.noreply.github.com>
2025-02-25 20:12:48 +0100

    chat1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,jsx}"
-    ],
-    darkMode: "class", // 🔥 ATTIVA IL TEMA SCURO
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
-  
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}"
+  ],
+  darkMode: "class", // 🔥 ATTIVIAMO IL TEMA SCURO
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
